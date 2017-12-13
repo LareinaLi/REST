f0f38fd2c16cc5561f50770e111434a5f424cc65
LareinaLi <32418242+LareinaLi@users.noreply.github.com>
2017-12-13 21:34:24 +0800

    Update master.py

--- master.py
+++ master.py
@@ -1,6 +1,9 @@
 import commands
+import jason
+import time
+import request
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,20 +11,57 @@ api = Api(app)
 task = []
 
 
-class Master():
+class Complexity(Resource):
     def __init__(self):
-        self.startTime = 0 
+        self.master = m
+        self.parser = reqparse.RequestParser()
+        self.reqparser.add_argument('file', type=str, location='json')
+        self.reqparser.add_argument('complexity', type=str, location='json')
+
+    def get(self):
+        if self.master.workerNum < self.master.workerSum:
+            return {'file': 'wait'}
+        if len(self.master.file) == 0:
+            return {'file': 'no file'}
+        else:
+            fileid = self.master.file[0]
+            del self.master.file[0]
+            return {'file': fileid}
+
+    def put(self):
+        if len(self.master.fileComplexity) == self.master.fileNum:
+            endTime = time() - self.master.startTime
+            totalComplexity = 0
+            for c in self.master.fileComplexity:
+                if float(c['complexity']) > 0:
+                    totalComplexity += float(c['complexity'])
+            aveComplexity = totalComplexity / len(self.master.complexity)
+            print('Average complexity: ', aveComplexity, '\n')
+            print('Calculate Time: ', endTime, ' seconds\n')
+
+
+class Master:
+    def __init__(self):
+        self.startTime = 0.0
+        self.workerTotal = int(input('The number of worker you need: '))
         self.workerNum = 0
+        response = requests.get('url to github repo')
+        data = json.loads(response)
+        for d in data:
+            self.file.append(d['id'])
+        self.file = []
+        self.complexity = []
+        self.fileNum = len(self.file)
+        print('The number of files: ', self.fileNum)
 
     def get(self):
         return {'hello': 'world'}
 
-    cmdline = 'radon cc ' + filename + ' -a'
-    result = commands.getoutput(cmdline)
-    print(result)
+
 
 
 api.add_resource(Master, '/')
 
 if __name__ == '__main__':
+    m = Master()
     app.run(port=9999)
